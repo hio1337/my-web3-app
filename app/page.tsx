dec7d19d72022d45497fac87e4352e85f518d0c0
hio1337 <andreypryakhin69@gmail.com>
2026-02-06 14:39:49 +1000

    UI fixes, added <footer/>

--- app/page.tsx
+++ app/page.tsx
@@ -17,30 +17,68 @@ export default function Wallet() {
     : "—";
 
   return (
-    <div className="relative min-h-screen overflow-hidden">
-      <div>
+    <div className="relative min-h-screen flex flex-col overflow-hidden">
+      <main className="grow">
         <div>
-          <ReadContract />
-        </div>
-        <div className="m-2">
-          <ConnectKitButton />
-        </div>
-        {isConnected && (
-          <div className="flex items-center justify-center bg-white w-24 h-10 mt-2 ml-2 rounded-xl">
-            <p className="text-[#414451] font-medium text-1xl">
-              {formattedBalance} ETH
-            </p>
+          <div>
+            <ReadContract />
           </div>
-        )}
-      </div>
-      <div className="flex absolute top-1/2 left-1/2 -translate-1/2 items-center space-x-5">
-        <div className="flex items-center">
-          <SendTransaction />
+          <div className="m-2">
+            <ConnectKitButton />
+          </div>
+          {isConnected && (
+            <div className="flex items-center justify-center bg-white w-24 h-10 mt-2 ml-2 rounded-xl">
+              <p className="text-[#414451] font-medium text-1xl">
+                {formattedBalance} ETH
+              </p>
+            </div>
+          )}
         </div>
-        <div>
-          <WriteToContract />
+        <div className="flex absolute top-1/2 left-1/2 -translate-1/2 items-center space-x-5">
+          <div className="flex items-center">
+            <SendTransaction />
+          </div>
+          <div>
+            <WriteToContract />
+          </div>
         </div>
-      </div>
+      </main>
+
+      <footer className="relative bg-linear-to-r from-blue-600 to-indigo-600 text-gray-300 py-3">
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center gap-6">
+            <div>
+              <p className="text-lg font-semibold text-white/90">My Web3 App</p>
+              <p className="mt-1 text-sm">
+                Created via Next.js, React, Wagmi, ConnectKit
+              </p>
+            </div>
+
+            <div className="flex absolute left-1/2 -translate-x-1/2 gap-2 text-sm">
+              <a
+                href="https://github.com/hio1337/my-web3-app"
+                className="hover:text-white transition-colors"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://t.me/twintractionbeam"
+                className="hover:text-white transition-colors"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Telegram
+              </a>
+            </div>
+
+            <div className="text-sm opacity-75">
+              © {new Date().getFullYear()} Built with ❤️
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
